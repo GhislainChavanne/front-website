6c92f3bc06774acfc6ac2a33c499dc1590d548f7
Ghislain Chavanne <gh.chavanne@gmail.com>
2016-12-13 17:43:48 +0100

    meta test

--- app/assets/config/environment.rb
+++
@@ -1,9 +0,0 @@
-# Load the Rails application.
-require_relative 'application'
-
-# Initialize the Rails application.
-Rails.application.initialize!
-
-
-# Initialize default meta tags.
-DEFAULT_META = YAML.load_file(Rails.root.join('config/meta.yml'))
